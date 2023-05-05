363f23db0e4305d22a1c229bf181bff6533b51a2
Ryo <matsukawa.ryo42@gmail.com>
2023-05-05 11:38:52 +0900

    add: ch3 HTTPのハンズオン

---
+++ sandbox/ch3/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const api = require("./routes/api");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use("/api", api);
+
+app.get("/", (req, res, next) => {
+  res.end("Top Page");
+})
+
+// サーバを起動する
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+})
