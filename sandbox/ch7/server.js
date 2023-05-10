0bb9940271171e8aa2721f3a94a1d1034baef4f4
Ryo <matsukawa.ryo42@gmail.com>
2023-05-10 10:56:45 +0900

    アカウント作成フォーム

---
+++ sandbox/ch7/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const api = require("./routes/api");
+const csrf = require("./routes/csrf");
+
+const crypto = require("crypto");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public", {
+  setHeaders: (res, path, stat) => {
+    res.header("X-Frame-Options", "DENY");
+  }
+}));
+
+app.use(express.static("public"));
+app.use("/api", api);
+app.use("/csrf", csrf);
+
+app.get("/", (req, res, next) => {
+  res.end("Top Page");
+})
+
+app.get("/csp", (req, res, next) => {
+  const nonceValue = crypto.randomBytes(16).toString("base64");
+
+  res.header("Content-Security-Policy",
+              `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+              "object-src 'none';" +
+              "base-uri 'none';" +
+              "require-trusted-types-for 'script';"
+  );
+
+  res.render("csp", { nonce: nonceValue });
+});
+
+// フォームの内容を解析してreq.bodyへ格納する
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  res.send("アカウント登録しました。");
+});
+
+// サーバを起動する
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+})
